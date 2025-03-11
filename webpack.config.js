6f2e166981821605ced62d12808698143f64f640
nto300002 <samonkntd@gmail.com>
2025-03-11 14:06:07 +0900

    package

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/renderer/index.tsx',
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist/renderer'),
+    filename: 'renderer.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/renderer/index.html',
+    }),
+  ],
+};
